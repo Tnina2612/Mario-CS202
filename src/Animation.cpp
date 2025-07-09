331c4b7c3f2ce7b3a4207c1939a91e3159c89baa
Tuanlinh1405 <buituanlinh63@gmail.com>
2025-07-09 21:56:42 +0700

    delete Character.h

--- src/Animation.cpp
+++ src/Animation.cpp
@@ -1,2 +1,15 @@
 #include"../include/entities/Animation.hpp"
 
+Animation::Animation() : currentFrame(0), frameTime(0.2f) {}
+Animation::Animation(const vector<Rectangle>& frames, const Texture2D& sprite, float frameTime)
+    : frames(frames), currentFrame(0), sprite(sprite), frameTime(frameTime) {}
+
+void Animation::setFrames(const vector<Rectangle>& newFrames) {
+    frames = newFrames;
+    currentFrame = 0; // Reset to the first frame
+}
+
+void Animation::update(float deltaTime, int startFrame, int size) {
+    if(frames.empty()) return;
+    
+}
