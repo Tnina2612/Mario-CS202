8edeec6cb8d914f868410fa9833cdb2580b6cf87
Tuanlinh1405 <buituanlinh63@gmail.com>
2025-07-11 21:25:02 +0700

    move item

--- assets/images/Coordinate.h
+++ assets/images/Coordinate.h
@@ -235,3 +235,47 @@ namespace Enemy {
         };
     };
 };
+
+namespace Item
+{
+    //Line 1
+    const Rectangle BLOCK_QUESTION =          {4, 4, 16, 16};
+    const Rectangle BLOCK_QUESTION_FLASHING = {34, 4, 16, 16};
+    const Rectangle BLOCK_QUESTION_USED =     {64, 4, 16, 16};
+    const Rectangle SPRINGBOARD_EXTENDED =    {94, 0, 16, 24};
+    const Rectangle SPRINGBOARD =             {124, 4, 16, 16};
+    const Rectangle SPRINGBOARD_COMPRESSED =  {154, 8, 16, 8};
+    const Rectangle BEANSTALK_FRAME_1 =       {188, 7, 8, 9};
+    const Rectangle BEANSTALK_FRAME_2 =       {215, 4, 14, 16};
+    const Rectangle CHAR_TOAD =               {244, 0, 16, 24};
+
+    //Line 2
+    const Rectangle PLATFORM_MOVING_3 = {0, 38, 24, 8};
+    const Rectangle PLATFORM_MOVING_4 = {26, 38, 32, 8};
+    const Rectangle PLATFORM_MOVING_6 = {63, 38, 48, 8};
+    const Rectangle COINS =             {123, 38, 48, 8};
+    const Rectangle MUSHROOM_SUPER =    {184, 34, 16, 16};
+    const Rectangle MUSHROOM_ONE_UP =   {214, 34, 16, 16};
+
+    //Line 3
+    const Rectangle FIRE_FLOWER_ANIMATION_1_FRAME_1 = {4, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_1_FRAME_2 = {34, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_1_FRAME_3 = {64, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_1_FRAME_4 = {94, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_2_FRAME_1 = {124, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_2_FRAME_2 = {154, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_2_FRAME_3 = {184, 64, 16, 16};
+    const Rectangle FIRE_FLOWER_ANIMATION_2_FRAME_4 = {214, 64, 16, 16};
+
+    //Line 4
+    const Rectangle SUPER_STAR_FRAME_1 = {5, 94, 14, 16};
+    const Rectangle SUPER_STAR_FRAME_2 = {35, 94, 14, 16};
+    const Rectangle SUPER_STAR_FRAME_3 = {65, 94, 14, 16};
+    const Rectangle SUPER_STAR_FRAME_4 = {95, 94, 14, 16};
+    const Rectangle COIN_SPIN_FRAME_1 =  {128, 95, 8, 14};
+    const Rectangle COIN_SPIN_FRAME_2 =  {160, 95, 4, 14};
+    const Rectangle COIN_SPIN_FRAME_3 =  {191, 95, 1, 14};
+    const Rectangle COIN_SPIN_FRAME_4 =  {220, 95, 4, 14};
+    const Rectangle PRINCESS_PEACH =     {245, 90, 14,24};
+    
+} 
