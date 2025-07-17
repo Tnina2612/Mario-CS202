bccca156fae7a65f1df2c1fd79caccc7f4390782
Tuanlinh1405 <buituanlinh63@gmail.com>
2025-07-17 22:56:22 +0700

    interface of singleton

--- include/core/InputManager.hpp
+++ include/core/InputManager.hpp
@@ -1,8 +1,14 @@
 #include "raylib.h"
 #include <vector>
+#include <unordered_map>
+#include "Singleton.h"
 using namespace std;
 
 
 class InputManager {
+    friend class Singleton;
+private:
+    
+public:
     
 };
