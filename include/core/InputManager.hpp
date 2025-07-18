a38eedd1707c2ad2b314471f146b2d8e09d2fc85
Phát Hoàng Đức Hưng <hdhphat2421@apcs.fitus.edu.vn>
2025-07-18 23:06:10 +0700

    Revert "interface of singleton"
    This reverts commit bccca156fae7a65f1df2c1fd79caccc7f4390782.

--- include/core/InputManager.hpp
+++ include/core/InputManager.hpp
@@ -1,14 +1,8 @@
 #include "raylib.h"
 #include <vector>
-#include <unordered_map>
-#include "Singleton.h"
 using namespace std;
 
 
 class InputManager {
-    friend class Singleton;
-private:
-    
-public:
     
 };
