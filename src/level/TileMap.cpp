d21ca0357ce6410e18396691b36075fa658c216d
Phát Hoàng Đức Hưng <hdhphat2421@apcs.fitus.edu.vn>
2025-07-25 10:12:27 +0700

    Try to merge code blocks and character

--- src/level/TileMap.cpp
+++ src/level/TileMap.cpp
@@ -36,44 +36,44 @@ void TileMap::draw(void) {
 void TileMap::update(std::shared_ptr<Character> player) {
     if(player->getPos().x <= 0) player->hitBlockLeft();
     if(player->getPos().x + player->getRectangle().width >= width * BLOCKSIDE) player->hitBlockRight();
+    const Rectangle& charRec = Rectangle{player->getPos().x, player->getPos().y, 16.f, 32.f};//player->getRectangle();
     for(int i = 0; i < height; i++) {
         for(int j = 0; j < width; j++) {
             if(tiles[i][j] == nullptr) continue;
-    //         const Rectangle& charRec = player->getRectangle();
-    //         const Rectangle& blockRec = blockTiles[i][j]->getRectangle();
+            const Rectangle& blockRec = tiles[i][j]->getRectangle();
 
             // Player collisions on ground
-    //         if(charRec.y + charRec.height == blockRec.y &&
-    //            blockRec.x <= charRec.x + charRec.width &&
-    //            charRec.x <= blockRec.x + blockRec.width) {
-                // player->hitBlockBottom();
-    //         } 
+            if(charRec.y + charRec.height == blockRec.y &&
+               blockRec.x <= charRec.x + charRec.width &&
+               charRec.x <= blockRec.x + blockRec.width) {
+                player->hitBlockBottom();
+            } 
             // Player head-bumps a block
-    //         else if(charRec.y == blockRec.y + blockRec.height &&
-    //             blockRec.x <= charRec.x + charRec.width && 
-    //             charRec.x <= blockRec.x + blockRec.width) {
-                // player->hitBlockTop();
-    //             if(player->isSmall()) {
-    //                 blockTiles[i][j]->jiggles();
-    //             }
-    //             else {
-    //                 if(blockTiles[i][j]->break()) {
-    //                     blockTiles[i][j].reset(); // i.e. blockTiles[i][j] = nullptr;
-    //                 }
-    //             }
-    //         }
+            else if(charRec.y == blockRec.y + blockRec.height &&
+                blockRec.x <= charRec.x + charRec.width && 
+                charRec.x <= blockRec.x + blockRec.width) {
+                player->hitBlockTop();
+                if(player->getCharacterState() == 0) {
+                    tiles[i][j]->jiggle();
+                }
+                else {
+                    if(tiles[i][j]->breakBrick()) {
+                        tiles[i][j].reset(); // i.e. blockTiles[i][j] = nullptr;
+                    }
+                }
+            }
             // Player collisions left
-    //         else if(charRec.x == blockRec.x + blockRec.width &&
-    //         blockRec.y <= charRec.y + charRec.height &&
-    //         charRec.y <= blockRec.y + blockRec.height) {
-    //             player->hitBlockLeft();
-    //         }
+            else if(charRec.x == blockRec.x + blockRec.width &&
+            blockRec.y <= charRec.y + charRec.height &&
+            charRec.y <= blockRec.y + blockRec.height) {
+                player->hitBlockLeft();
+            }
             // Player collision right
-    //         else if(charRec.x + charRec.width == blockRec.x &&
-    //         blockRec.y <= charRec.y + charRec.height &&
-    //         charRec.y <= blockRec.y + blockRec.height) {
-    //             player->hitBlockRight();
-    //         }
+            else if(charRec.x + charRec.width == blockRec.x &&
+            blockRec.y <= charRec.y + charRec.height &&
+            charRec.y <= blockRec.y + blockRec.height) {
+                player->hitBlockRight();
+            }
         }
     }
 }
